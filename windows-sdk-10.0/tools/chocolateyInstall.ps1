db06d1a463cc211aa2424aad33e7924a4d812a0a
Jakub Bereżański <kuba@berezanscy.pl>
2015-12-14 21:57:23 +0100

    windows-sdk-10.0: update to 10.0.10586.15 (Windows 10 release 1511)

--- windows-sdk-10.0/tools/chocolateyInstall.ps1
+++ windows-sdk-10.0/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'windows-sdk-10.0'
 $installerType = 'EXE'
-$url = 'http://download.microsoft.com/download/E/1/F/E1F1E61E-F3C6-4420-A916-FB7C47FBC89E/standalonesdk/sdksetup.exe'
+$url = 'http://download.microsoft.com/download/6/A/2/6A2ECE81-C934-4E47-91CC-52DA00A65345/sdksetup.exe'
 $silentArgs = "/Quiet /NoRestart /Log ""$env:temp\$packageName.log"""
 $validExitCodes = @(0,3010)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
